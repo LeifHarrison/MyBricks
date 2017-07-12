d3d2010372814aa57fec2328a59b01261b835b59
Leif Harrison <lharrison@deloitte.com>
2017-07-12 12:29:05 -0700

    Initial Commit

---
+++ MyBricks/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyBricks
+//
+//  Created by Leif Harrison on 7/12/17.
+//  Copyright © 2017 Catsreach. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
