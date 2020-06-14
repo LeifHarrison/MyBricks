debd8d87af2f3992e9ffed92bfd23a69c78e233c
Leif Harrison <leif.harrison@gmail.com>
2020-06-13 20:42:23 -0700

    Remove API keys/ids from Constants file

--- MyBricks/Common/Constants.swift
+++ MyBricks/Common/Constants.swift
@@ -15,25 +15,12 @@ struct Constants {
     struct Brickset {
         static let url = "https://brickset.com"
         static let signupURL = "https://brickset.com/signup"
-        static let apiKey = "PJ6U-J8Ob-GG1k"
+        static let apiKey = "<Brickset API Key>"
     }
     
     struct Rebrickable {
         static let url = "https://rebrickable.com"
-        static let apiKey = "7ee6c70b29646296c7d4778cabb4d476"
-    }
-    
-    struct GoogleMobileAds {
-        static let appId = "ca-app-pub-2158913963073917~7545030919"
-        
-        struct AdUnits {
-#if DEBUG
-            static let browseThemes = "ca-app-pub-3940256099942544/2934735716" // Test Banner Ad ID
-#else
-            static let browseThemes = "ca-app-pub-2158913963073917/8970503662" // Production Banner Ad ID
-#endif
-        }
-        
+        static let apiKey = "<Rebrickable API Key>"
     }
     
     struct Alamofire {
